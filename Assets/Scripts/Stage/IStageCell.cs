d931be258d703b4a89bb03b7781fc3b0b255c37f
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-10-24 20:29:09 +0900

    [update] Stageの調整。

--- Assets/Scripts/Stage/IStageCell.cs
+++ Assets/Scripts/Stage/IStageCell.cs
@@ -1,10 +1,11 @@
 //日本語対応
+using TeamB_TD.Player;
 
 namespace TeamB_TD
 {
     namespace Stage
     {
-        public interface IStageCell
+        public interface IStageCell : IFocusable
         {
             CellStatus Status { get; }
         }
